33cdafaf2734fe3bf65e74a9b8820ceec77a8a8a
mazhar5 <mazhar@digital-dividend.com>
2022-01-24 12:37:47 +0500

    next js example

---
+++ components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from "next/link";
+function Nav(props) {
+    return (
+        <nav>
+            <ul>
+                <li>
+                <Link href="/">Home</Link>
+                </li>
+                <li>
+                <Link href="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head'
+function about(props) {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+            </Head>
+                <h1>About</h1>
+        </div>
+    );
+}
+
+export default about;
